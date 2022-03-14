15d37464b93dfbed11aa28a7b6c7ec9938b67639
Robin Söderberg <robsod94@gmail.com>
2022-03-14 10:36:41 +0100

    new version 2.0

--- MazeSimulation.java
+++ MazeSimulation.java
@@ -1,4 +1,3 @@
-//package gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
@@ -8,7 +7,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -21,28 +19,14 @@ import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-/*
-import model.Maze;
-import model.MemoryRobot;
-import model.Position;
-import model.RandomRobot;
-import model.LeftHandRuleRobot;
-import model.Robot;
-*/
-
-
 /**
- * Graphical robot simulator
- * @author johane
- *
+ * Graphical simulator for a robot in a maze.
+ * @author Robin Söderberg
+ * @date 2022-03-14
  */
 public class MazeSimulation {
 
     public enum RobotType {Random,Memory,LeftHand}
-
-    /**
-     * Maximal number of steps that the simulation will run
-     */
     public static final int MAX_STEPS=100;
     private DisplayPanel[][] mazeDisplay;
     private Maze maze;
@@ -50,12 +34,18 @@ public class MazeSimulation {
     private JPanel mazePanel;
 
     /**
-     *
+     * @param args Arguments to the program.
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(MazeSimulation::new);
+    }
+
+    /**
+     * Create th window
      */
     public MazeSimulation() {
 
-        JFrame theWindow;
-        theWindow = setupMainWindow();
+        JFrame theWindow = setupMainWindow();
         JMenuBar menuBar = createMenuBar();
         theWindow.setJMenuBar(menuBar);
 
@@ -68,239 +58,64 @@ public class MazeSimulation {
         theWindow.add(topPanel,BorderLayout.NORTH);
         theWindow.setVisible(true);
         loadMaze();
-
-
     }
 
 
     /**
-     * Create and configure the menubar for the application
-     * @return the configured menu bar redy to be added to the userinterface.
+     * Function will create the menubar for the application.
+     * @return the configured menubar.
      */
     protected JMenuBar createMenuBar() {
-        JMenuBar menuBar=new JMenuBar();
-        JMenu file=new JMenu("File");
-        JMenuItem open=new JMenuItem("Open");
-        open.addActionListener(e->this.loadMaze());
-        JMenuItem quit=new JMenuItem("Quit");
-        quit.addActionListener(e->System.exit(0));
-        file.add(open);
-        file.add(quit);
-        JMenu robot=new JMenu("Robot");
-        menuBar.add(file);
-        menuBar.add(robot);
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Menu");
+        JMenuItem newFile = new JMenuItem("New file");
+        newFile.addActionListener(e->this.loadMaze());
+        JMenuItem Exit = new JMenuItem("Exit");
+        Exit.addActionListener(e->System.exit(0));
+        menu.add(newFile);
+        menu.add(Exit);
+        menuBar.add(menu);
         return menuBar;
     }
 
     /**
-     * Create the top part of the user interface with options to select which
-     * type of robot to simulate
-     * @return panel containing this part of the userinterface
+     * function will create buttons so the user can select what
+     * type of robot it wants to use.
+     * @return panel that contains all the buttons.
      */
     protected JPanel createTopPanel() {
-        JPanel topPanel=new JPanel();
+        JPanel topPanel = new JPanel();
         topPanel.setLayout(new GridLayout(1,0));
-        JButton randomButton = new JButton("RandomRobot");
+
+        /* Buttons */
+        JButton leftHandButton = new JButton("LeftHand Robot");
+        topPanel.add(leftHandButton);
+        JButton memoryButton = new JButton("Memory Robot");
+        topPanel.add(memoryButton);
+        JButton randomButton = new JButton("Random Robot");
         topPanel.add(randomButton);
-        JButton memButton = new JButton("MemoryRobot");
-        topPanel.add(memButton);
-        JButton leftButton = new JButton("LeftHandRuleRobot");
-        topPanel.add(leftButton);
+
+        /* Button to clear the field */
         JButton clearButton = new JButton("Clear");
         clearButton.addActionListener(e->setupMazePanel());
         topPanel.add(clearButton);
 
-        //Configure button actions
-        ActionListener randomRobot=new RobotButtonListener(
-                RobotType.Random);
+        /* Button actions */
+        ActionListener randomRobot=new RobotButtonListener(RobotType.Random);
         randomButton.addActionListener(randomRobot);
-        ActionListener memRobot=new RobotButtonListener(
-                RobotType.Memory);
-        memButton.addActionListener(memRobot);
-        ActionListener leftRobot=new RobotButtonListener(
-                RobotType.LeftHand);
-        leftButton.addActionListener(leftRobot);
+        ActionListener memRobot=new RobotButtonListener(RobotType.Memory);
+        memoryButton.addActionListener(memRobot);
+        ActionListener leftRobot=new RobotButtonListener(RobotType.LeftHand);
+        leftHandButton.addActionListener(leftRobot);
 
         return topPanel;
     }
 
     /**
-     * Prompts the user for a file to open and displays that maze file in the
-     * gui
-     */
-    protected void loadMaze() {
-        JFileChooser fileChooser;
-        File file=null;
-        boolean done=false;
-        //Load maze
-        fileChooser=new JFileChooser();
-        FileFilter filter = new FileNameExtensionFilter("Maze file", "maze");
-        fileChooser.setFileFilter(filter);
-        do {
-            int returnVal=fileChooser.showOpenDialog(null);
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                file = fileChooser.getSelectedFile();
-            } else {
-                System.exit(0); //Canceled by user
-            }
-            try {
-                //Create a maze from a text file
-                maze=(new Maze(new FileReader(file)));
-                mazeDisplay=new DisplayPanel[maze.getNumRows()][maze.getNumCulum()];
-                mazePanel.setLayout(new GridLayout(maze.getNumRows(),maze.getNumCulum()));
-                done=true;
-            } catch (FileNotFoundException e) {
-                updateMessage("Unable to open maze file");
-            } catch (IOException e) {
-                updateMessage("Unable to read maze file");
-            }
-            catch (Exception e) {
-                updateMessage("Something wrong with map");
-            }
-        } while(!done);
-        setupMazePanel();
-    }
-
-    /**
-     * Create and configure the main window
-     * @return the window
-     */
-    protected JFrame setupMainWindow() {
-        JFrame theWindow;
-        //Setup main window
-        theWindow = new JFrame("RoboSim");
-        theWindow.setSize(500, 500);
-        theWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        theWindow.setBackground(Color.WHITE);
-        return theWindow;
-    }
-
-    /**
-     * Display maze in window
-     */
-    private void setupMazePanel() {
-        mazePanel.removeAll();
-        for(int i=0;i<maze.getNumRows();i++) {
-            for(int j=0;j<maze.getNumCulum();j++) {
-                DisplayPanel dp=new DisplayPanel();
-                if(!maze.isMovable(new Position(j,i)))
-                    dp.setWall();
-                else if(maze.isGoal(new Position(j,i)))
-                    dp.setGoal();
-                else
-                    dp.setEmpty();
-                mazePanel.add(dp);
-                mazeDisplay[i][j]=dp;
-            }
-        }
-        mazePanel.doLayout();
-    }
-
-    /**
-     * Simulate a robot moving in the maze
-     * @param r the robot to simulate
-     */
-    public void runSimulation(Robot r) {
-
-        Position robotPos=r.getPos();
-        displayRobot(robotPos);
-        int i=0;
-        do { //Simulate the robot moving
-            removeRobot(r.getPos());
-            r.move();
-            displayRobot(r.getPos());
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                return;
-            }
-            i++;
-        } while(!r.ReachedGoal() && i<MAX_STEPS);
-        if(r.ReachedGoal())
-            updateMessage("Robot reached goal after "+i+" steps");
-        else
-            updateMessage("Robot did not reach goal before reaching maximum number of steps");
-    }
-
-    /**
-     * Notify the user about something. May be called on any thread
-     * @param string the message to the user
-     */
-    private void updateMessage(final String string) {
-        SwingUtilities.invokeLater(()->messageField.setText(string));
-    }
-
-    /**
-     * Remove the robot from the specified position in the gui
-     * @param pos the position were a robot should be removed
-     */
-    private void removeRobot(final Position pos) {
-        SwingUtilities.invokeLater(
-                ()->mazeDisplay[pos.getY()][pos.getX()].setEmpty());
-    }
-
-    /**
-     * Display a robot in the given position
-     * @param robotPos
-     */
-    private void displayRobot(final Position robotPos) {
-        SwingUtilities.invokeLater(
-                ()-> mazeDisplay[robotPos.getY()][robotPos.getX()].setRobot());
-    }
-
-    class RobotButtonListener implements ActionListener {
-
-        private MazeSimulation.RobotType type;
-
-        /**
-         * @param r
-         */
-        public RobotButtonListener(MazeSimulation.RobotType r) {
-            type=r;
-        }
-
-        /* (non-Javadoc)
-         * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-         */
-        @Override
-        public void actionPerformed(ActionEvent event) {
-            new Thread(new Runnable(){
-
-                @Override
-                public void run() {
-                    try {
-                        switch(type) {
-                            case Memory:
-                                runSimulation(new MemoryRobot(maze));
-                                break;
-                            case LeftHand:
-                                runSimulation(new LeftHandRuleRobot(maze));
-                                break;
-                            case Random:
-                                runSimulation(new RandomRobot(maze));
-                                break;
-                        }
-                    } catch(Exception e) {
-                        updateMessage("Simulation failed because of "+
-                                e.getClass().getName());
-                    }
-
-                }}).start();
-        }
-    }
-
-    /**
-     * A simple panel able to display one of the things available in a simulation
-     * of a robot walking in a maze. In this simple version the different objects
-     * are only displayed as different colored tiles
-     * @author johane
-     *
+     * Set colors.
      */
     public static class DisplayPanel extends JPanel {
 
-        /**
-         * ID for serialization
-         */
         private static final long serialVersionUID = 1L;
 
         public void setWall() {
@@ -322,9 +137,161 @@ public class MazeSimulation {
     }
 
     /**
-     * @param args
+     * Function will ask the user for a maze file.
      */
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(()->new MazeSimulation());
+    protected void loadMaze() {
+        File file = null;
+        boolean done = false;
+
+        JFileChooser fileChooser = new JFileChooser();
+        FileFilter filter = new FileNameExtensionFilter("Maze file", "maze");
+        fileChooser.setFileFilter(filter);
+
+        do {
+            int returnVal = fileChooser.showOpenDialog(null);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                file = fileChooser.getSelectedFile();
+            } else {
+                /* If the user are not giving any file */
+                System.exit(0);
+            }
+            try {
+                /* Reading the the maze map */
+                maze = (new Maze(new FileReader(file)));
+                mazeDisplay = new DisplayPanel[maze.getNumRows()][maze.getNumCulum()];
+                mazePanel.setLayout(new GridLayout(maze.getNumRows(), maze.getNumCulum()));
+                done=true;
+            } catch (FileNotFoundException e) {
+                updateMessage("Unable to open maze file");
+            } catch (IOException e) {
+                updateMessage("Unable to read maze file");
+            }
+            catch (Exception e) {
+                updateMessage("Something wrong with map");
+            }
+        } while(!done);
+
+        setupMazePanel();
+    }
+
+    /**
+     * Create the window
+     * @return the window
+     */
+    protected JFrame setupMainWindow() {
+        JFrame theWindow;
+        theWindow = new JFrame("Maze");
+        theWindow.setSize(1000, 1000);
+        theWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return theWindow;
+    }
+
+    /**
+     * Print the maze in the window.
+     */
+    private void setupMazePanel() {
+        /* remove all the components if new map is loaded */
+        mazePanel.removeAll();
+        for(int i = 0; i < maze.getNumRows(); i++) {
+            for(int j = 0; j < maze.getNumCulum(); j++) {
+                DisplayPanel displayPanel = new DisplayPanel();
+                if(!maze.isMovable(new Position(j, i))) {
+                    displayPanel.setWall();
+                } else if(maze.isGoal(new Position(j, i))) {
+                    displayPanel.setGoal();
+                } else {
+                    displayPanel.setEmpty();
+                }
+
+                mazePanel.add(displayPanel);
+                mazeDisplay[i][j] = displayPanel;
+            }
+        }
+        mazePanel.doLayout();
+    }
+
+    /**
+     * Robot moving in the maze
+     * @param robot the specific robot in the maze.
+     */
+    public void runSimulation(Robot robot) {
+        /* Start position */
+        Position robotPos = robot.getPos();
+        /* Print robot in the maze */
+        displayRobot(robotPos);
+        int i = 0;
+
+        do {
+            /* remove the robot from the maze print */
+            removeRobot(robot.getPos());
+            robot.move();
+            /* Print the robot in the new position */
+            displayRobot(robot.getPos());
+            /* Make the thread sleep for 500ms to simulate steps*/
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                return;
+            }
+            i++;
+        } while(!robot.ReachedGoal() && i < MAX_STEPS);
+
+        if(robot.ReachedGoal())
+            updateMessage("Robot reached the goal after " + i + " steps");
+        else
+            updateMessage("Robot did not reach the goal before using the maximum number of steps");
+    }
+
+    /**
+     * Print out message to the user.
+     * @param string the message.
+     */
+    private void updateMessage(final String string) {
+        SwingUtilities.invokeLater(()->messageField.setText(string));
+    }
+
+    /**
+     * Remove the robot from the position in the maze.
+     * @param pos the position were the robot is.
+     */
+    private void removeRobot(final Position pos) {
+        SwingUtilities.invokeLater(()->mazeDisplay[pos.getY()][pos.getX()].setEmpty());
+    }
+
+    /**
+     * Print the robot in the specific position.
+     * @param robotPos the position were the robot is going to be printed.
+     */
+    private void displayRobot(final Position robotPos) {
+        SwingUtilities.invokeLater(()-> mazeDisplay[robotPos.getY()][robotPos.getX()].setRobot());
+    }
+
+    class RobotButtonListener implements ActionListener {
+
+        private MazeSimulation.RobotType type;
+
+        /**
+         * @param robot what type of robot
+         */
+        public RobotButtonListener(MazeSimulation.RobotType robot) {
+            type = robot;
+        }
+
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            new Thread(() -> {
+                try {
+                    switch (type) {
+                        case Memory -> runSimulation(new MemoryRobot(maze));
+                        case LeftHand -> runSimulation(new LeftHandRuleRobot(maze));
+                        case Random -> runSimulation(new RandomRobot(maze));
+                    }
+                } catch(Exception e) {
+                    updateMessage("Simulation failed because of "+ e.getClass().getName());
+                }
+
+            }).start();
+        }
     }
 }
